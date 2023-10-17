bed50eeea6a3ab269b0b8555791159dc37a9e4a4
Calvin Lu <calvinlu@live.com>
2023-10-16 21:28:29 -0700

    Update eval to discard prefs that do not exist

--- groups/evaluate_groups.py
+++ groups/evaluate_groups.py
@@ -11,6 +11,7 @@ def evaluate_groups(groups: list[list[Person]], people: list[Person]) -> float:
             continue
         
         fullfilled_prefs = 0
+        total_valid_prefs = 0
 
         for pref in person.prefs:
             pref_person = get_pref(people, pref.split(" ")[0], pref.split(" ")[1])
@@ -21,8 +22,13 @@ def evaluate_groups(groups: list[list[Person]], people: list[Person]) -> float:
 
             if search_groups(groups, person.first_name, person.last_name) != -1 and search_groups(groups, person.first_name, person.last_name) == search_groups(groups, pref_person.first_name, pref_person.last_name):
                 fullfilled_prefs += 1
+            
+            total_valid_prefs += 1
         
-        percent_of_pref_fufilled_sum += fullfilled_prefs / len(person.prefs)
+        if total_valid_prefs == 0:
+            continue
+        
+        percent_of_pref_fufilled_sum += fullfilled_prefs / total_valid_prefs
         total_people_with_pref += 1
 
     if total_people_with_pref == 0:
